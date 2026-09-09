0f0558fd8166cc29d94db6a5562f5d43094d77c7
Washington Brandao <wlbds43@gmail.com>
2026-09-09 17:22:18 -0300

    Configuracao e teste

---
+++ OS/os/src/main/java/com/brandao/os/services/DBService.java
@@ -0,0 +1,46 @@
+package com.brandao.os.services;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+import com.brandao.os.domain.Cliente;
+import com.brandao.os.domain.OS;
+import com.brandao.os.domain.Tecnico;
+import com.brandao.os.domain.enums.Prioridade;
+import com.brandao.os.domain.enums.Status;
+import com.brandao.os.repositories.ClienteRepository;
+import com.brandao.os.repositories.OSRepository;
+import com.brandao.os.repositories.TecnicoRepository;
+
+@Service
+public class DBService {
+	
+	@Autowired
+	private TecnicoRepository tecnoRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private OSRepository osRepository;
+	
+
+	public void instanciaDB() {
+		Tecnico t1 = new Tecnico(null, "Valdir Cezar", "598.769.120-62", "(88)98888-8888");
+		Cliente c1 = new Cliente(null, "Betina Campos", "598.508.200-80", "(88)98888-7777");
+		OS os = new OS(null, Prioridade.ALTA, "Teste create OS", Status.ANDAMENTO, t1, c1);
+
+		t1.getList().add(os);
+		c1.getList().add(os);
+
+		tecnoRepository.saveAll(Arrays.asList(t1));
+		clienteRepository.saveAll(Arrays.asList(c1));
+		osRepository.saveAll(Arrays.asList(os));
+	}
+
+	
+	
+	
+	
+}
